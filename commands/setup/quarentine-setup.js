f16796704bc3ddb90470e527e7e2cc7d30c2eb5d
Scope <github.scope.open@gmail.com>
2022-10-04 15:13:05 +1100

    j

--- commands/setup/quarentine-setup.js
+++ commands/setup/quarentine-setup.js
@@ -1,5 +1,6 @@
 const config = require('../../conf/botconfig.json')
 const enablers = require('../../conf/enablers.json')
+const embedConf = require('../../conf/embed.json')
 
 const client = require('discord.js')
 
@@ -10,6 +11,16 @@ module.exports = {
     usage: '',
     run: async (client, message, args) => {
         try {
+
+            if(embedConf.AUTHOR == "dependent") {
+
+            }
+
+            if(embedConf.AUTHORIMAGE == "dependent") {
+
+            }
+
+            // I believe whats above can be added to index.js to make it smaller, ill figure out
 
         } catch(err) {
             return message.reply(`There was an error \`${err.message}\``), console.log(`Error running, \n Err Message: ${err.message} \n Command Ran: ${message.content}`)
