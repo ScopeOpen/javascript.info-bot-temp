05c288031a42caf6f9498e56f1b6ab72a0419352
Scope <github.scope.open@gmail.com>
2022-10-05 22:52:20 +1100

    Stuff is done
    It's fully disgusting too look at and I could make so many improvements to this but I feel like it would be overdo and we can do that later on

---
+++ commands/other/ping.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'ping',
+	description: "Check bot's ping.",
+	cooldown: 3000,
+	userPerms: [],
+	botPerms: ["ATTACH_FILES"],
+	run: async (client, message, args) => {
+		const msg = await message.reply('Pinging...')
+		await msg.edit(`Pong! **${client.ws.ping} ms**`)
+	}
+};
--- util/events/message.js
+++ util/events/message.js
@@ -1,36 +1,63 @@
 const { Collection } = require("discord.js");
 const ms = require("ms");
 
-const config = require("../../config.json");
-const client = require("../../index.js");
+const config = require("../../config.json"), 
+	client = require("../../index.js");
 
 const prefix = config.Configuration.Prefix;
 const cooldown = new Collection();
 
 client.on("messageCreate", async (message) => {
-  if (message.content.bot) return;
-  if (message.channel.type !== 0) return;
-  if (!message.content.startsWith(prefix)) return;
+	if (message.content.bot) return;
+  	if (message.channel.type !== 0) return;
+  	if (!message.content.startsWith(prefix)) return;
+	const args = message.content.slice(prefix.length).trim().split(/ +/g);
+	const cmd = args.shift().toLowerCase();
+  	let command = client.commands.get(cmd);
+  	if (!command) command = client.commands.get(client.aliases.get(cmd));
 
-  let command = client.commands.get(cmd);
-  if (!command) command = client.commands.get(client.aliases.get(cmd));
 
-  const args = message.content.slice(prefix.length).trim().split(/ +/g);
-  const cmd = args.shift().toLowerCase();
+  	if (command) {
+		function embeds() {
+			command.botPerms.forEach((element) => {
+				if(!message.member.permissions.has.element) {
+					const USERPERMS = new EmbedBuilder()
+					.setDescription(`You don't have \`${command.userPerms}\` permissions to use this command!`)
+					.setColor('Red')
+					return message.reply({ embeds: [USERPERMS] })
+				}
+				if(!message.guild.members.cache.get(client.user.id).permissions.has.element) {
+					const BOTPERMS = new EmbedBuilder()
+					.setDescription(`I don't have \`${command.botPerms}\` permissions to use this command!`)
+					.setColor('Red')
+					return message.reply({ embeds: [BOTPERMS] })
+				}
+			})
+		}
 
-  if (command) {
-    if (command.cooldown) {
-      if (cooldown.has(`${command.name}-${message.author.id}`)) {
-        message.channel.send({ content: config.Configuration.Messages.COOLDOWN.replace( "<duration>", ms( cooldown.get(`${command.name}-${message.author.id}`) - Date.now(), { long: true } ) ), });
-        return;
-      }
+    		if (command.cooldown) {
+      		if (cooldown.has(`${command.name}-${message.author.id}`)) { message.channel.send({ content: config.Configuration.Messages.COOLDOWN.replace( "<duration>", ms( cooldown.get(`${command.name}-${message.author.id}`) - Date.now(), { long: true } ) ), }); return; }
+      
+			if (command.USERPERMS || command.BOTPERMS) {
+      			if (cooldown.has(`${command.name}-${message.author.id}`)) {
+					embeds()
+      			}
+      		}
 
-      if (command.USERPERMS || command.BOTPERMS) {
-        if (cooldown.has(`${command.name}-${message.author.id}`)) {
+			command.run(client, message, args)
+			cooldown.set(`${command.name}-${message.author.id}`, Date.now() + command.cooldown)
+			setTimeout(() => {
+				cooldown.delete(`${command.name}-${message.author.id}`)
+			}, command.cooldown);
+    		} else {
+			if(command.userPerms || command.botPerms) {
+				embeds()
+			}
+			command.run(client, message, args)
+		}
 
-        }
-
-      }
-    }
-  }
+		
+		
+  	}
 });
+
