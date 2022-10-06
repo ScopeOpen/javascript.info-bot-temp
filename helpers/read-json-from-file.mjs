b1d485ad98b20caf2a6df34ba199dd7295b3e3f6
ericchase <github@eric.addymail.com>
2022-10-06 15:24:09 -0500

    feat: readJSONFromFile helper function
    Signed-off-by: ericchase <github@eric.addymail.com>

---
+++ helpers/read-json-from-file.mjs
@@ -0,0 +1,17 @@
+import fs from "node:fs";
+
+export function readJSONFromFile(path) {
+  // https://nodejs.org/api/fs.html#fsreadfilesyncpath-options
+  return JSON.parse(fs.readFileSync(path));
+}
+
+// Tests
+// https://jasmine.github.io/index.html
+if (globalThis['describe']) {
+  describe("readJSONFromFile", function () {
+    it("reads the json file and returns the contents as string", function () {
+      const contents = readJSONFromFile('./test-directory/test-file.json')
+      expect(contents.hello).toBe('world!');
+    });
+  });
+}
