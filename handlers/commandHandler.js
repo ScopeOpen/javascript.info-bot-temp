5bc24224f5b8987d588482fa13321e3ec084cfb2
ericchase <github@eric.addymail.com>
2022-10-06 15:51:00 -0500

    refactor: to esm
    Signed-off-by: ericchase <github@eric.addymail.com>

--- handlers/commandHandler.js
+++ handlers/commandHandler.js
@@ -1,29 +1,48 @@
-const fs = require('fs');
-const chalk = require('chalk')
-const AsciiTable = require('ascii-table')
+import fs from "node:fs";
+import chalk from "chalk";
 
-var table = new AsciiTable()
-table.setHeading('Commands', 'Loaded').setBorder('║', '=', '.')
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
+const AsciiTable = require("ascii-table");
 
-module.exports = (client) => {
-	fs.readdirSync('./commands/').forEach(dir => {
-		const files = fs.readdirSync(`./commands/${dir}/`).filter(file => file.endsWith('.js'));
-		if(!files || files.legnth <= 0) console.log(chalk.red("Commands - 0"))
-				files.forEach((file) => {
-						let command = require(`../commands/${dir}/${file}`)
-						if(command) {
-								client.commands.set(command.NAME, command);
-								client.all.set(command)
-								if(command.ALIASES && Array.isArray(command.ALIASES)) {
-										command.ALIASES.forEach(alias => {
-												client.aliases.set(alias, command.NAME)
-										})
-								}
-								table.addRow(command.NAME, '✅')
-						} else {
-								table.addRow(file, '❌')
-						}
-				});
-	});
-	console.log(chalk.blue(table.toString()))
-};
+const table = new AsciiTable();
+table.setHeading("Events", "Loaded").setBorder("║", "=", ".");
+
+// export function buildCommandSet({ getFileTree }) {
+//   const entries = getFileTree("./commands");
+//   console.log(entries);
+//   return {};
+// }
+
+export async function handler(client, { _import, getFileTree }) {
+  const filepaths = getFileTree("./commands")
+    .filter((entry) => entry.isFile)
+    .map((entry) => entry.path);
+
+  if (filepaths.length === 0) {
+    console.log(chalk.red("Commands - 0"));
+  } else {
+    for (const filepath of filepaths) {
+      console.log(filepath);
+      const module = await _import(filepath);
+      const command = module.default;
+
+      if (command) {
+        client.commands.set(command.NAME, command);
+        client.all.set(command);
+
+        if (command.ALIASES && Array.isArray(command.ALIASES)) {
+          command.ALIASES.forEach((alias) => {
+            client.aliases.set(alias, command.NAME);
+          });
+        }
+
+        table.addRow(command.NAME, "✅");
+      } else {
+        table.addRow(file, "❌");
+      }
+    }
+
+    console.log(chalk.blue(table.toString()));
+  }
+}
